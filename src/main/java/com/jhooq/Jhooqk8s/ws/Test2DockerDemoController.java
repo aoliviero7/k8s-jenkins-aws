1c6692380c901074af2290672580e7ed755edd33
aoliviero7 <56033667+aoliviero7@users.noreply.github.com>
2023-05-13 15:49:11 +0200

    Update and rename JhooqDockerDemoController.java to Test2DockerDemoController.java

--- src/main/java/com/jhooq/Jhooqk8s/ws/Test2DockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/Test2DockerDemoController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class JhooqDockerDemoController {
+public class Test2DockerDemoController {
 
-    @GetMapping("/hello")
+    @GetMapping("/test2")
     public String hello() {
-        return "Hello - Jhooq-k8s i Have updated the message";
+        return "Hello test 2 - Jhooq-k8s i Have updated the message";
     }
 }
