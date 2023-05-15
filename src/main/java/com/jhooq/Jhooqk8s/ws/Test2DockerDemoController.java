e828251a61d3f7c62c889aef97106baede02c115
aoliviero7 <56033667+aoliviero7@users.noreply.github.com>
2023-05-15 21:51:36 +0200

    Update Test2DockerDemoController.java

--- src/main/java/com/jhooq/Jhooqk8s/ws/Test2DockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/Test2DockerDemoController.java
@@ -17,7 +17,7 @@ public class Test2DockerDemoController {
     }
     
      @GetMapping("/test2/secondo")
-    public String test2() {
+    public String testsecondo() {
         return "Hello test test2/secondo 2 - Jhooq-k8s i Have updated the message";
     }
     
