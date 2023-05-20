2aa6828feffbb12ae22ba1ee28de081d9f5eccfb
aoliviero7 <56033667+aoliviero7@users.noreply.github.com>
2023-05-20 16:00:23 +0200

    Update DataDockerDemoController.java

--- src/main/java/com/jhooq/Jhooqk8s/ws/DataDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/DataDockerDemoController.java
@@ -11,6 +11,48 @@ public class DataDockerDemoController {
         return "Hello data manager- Jhooq-k8s i Have updated the message";
     }
     
+    @GetMapping("/data/{tableName}")
+    public String getAllEntriesAsJson(@PathVariable String tableName) {
+        List<Map<String, Object>> entries = new ArrayList<>();
+
+        String jdbcUrl = "jdbc:mysql://mysql-testtesi.ccl6bjosmzgx.eu-west-1.rds.amazonaws.com:3306/dbTesi";
+        String username = "userDB";
+        String password = "userDB";
+
+        try (Connection conn = DriverManager.getConnection(jdbcUrl, username, password)) {
+            String sql = "SELECT * FROM " + tableName;
+
+            try (Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery(sql)) {
+
+                ResultSetMetaData metaData = rs.getMetaData();
+                int columnCount = metaData.getColumnCount();
+
+                while (rs.next()) {
+                    Map<String, Object> entry = new HashMap<>();
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = metaData.getColumnName(i);
+                        Object value = rs.getObject(i);
+                        entry.put(columnName, value);
+                    }
+                    entries.add(entry);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String jsonResult = "";
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            jsonResult = objectMapper.writeValueAsString(entries);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return jsonResult;
+    }
+    
     @GetMapping("/")
     public String root() {
         return "ROOT";
